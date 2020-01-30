9010fad15d09331a0b93619b5d7209b5f5224967
Fannar <Fannar90@simnet.is>
2020-01-30 20:37:38 +0000

    klart

--- max_int.py
+++ max_int.py
@@ -13,13 +13,13 @@ while (north_int + south_int + east_int + west_int) >0:
         if north_int <= max_batch:
             north_int = 0
         else:
-            north_int = north_int + max_batch
+            north_int = north_int - max_batch
     
 
         if south_int <= max_batch:
             south_int = 0
         else:
-            south_int = south_int + max_batch
+            south_int = south_int - max_batch
 
     else:
         print("Green light on E/W")
@@ -27,13 +27,14 @@ while (north_int + south_int + east_int + west_int) >0:
         if east_int <= max_batch:
             east_int = 0
         else:
-            east_int = east_int + max_batch
+            east_int = east_int - max_batch
 
 
         if west_int <= max_batch:
             west_int = 0
         else:
-            west_int = west_int + max_batch
+            west_int = west_int - max_batch
 
          
-        print("No cars waiting, the traffic jam has been solved!")
+      
+print("No cars waiting, the traffic jam has been solved!")
