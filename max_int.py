dc86bffa9aa955ef3627be99f71583823584ad6b
Fannar <Fannar90@simnet.is>
2020-01-30 13:40:47 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -3,4 +3,24 @@ south_int = int(input("Number of cars travelling south: "))
 east_int = int(input("Number of cars travelling east: "))
 west_int = int(input("Number of cars travelling west: "))
 
+max_batch = 5
+
+cars_north = north_int
+print("Number of cars travelling north:",cars_north)
+cars_south = south_int
+print("Number of cars travelling south:",cars_south)
+cars_east = east_int
+print("NUmber of cars travelling east:",cars_east)
+cars_west = west_int
+print("Number of cars travelling west:",cars_west)
+
+n_s = cars_north + cars_south
+print("Cars travelling north and south",n_s)
+e_w = cars_east + cars_west
+print("Cars travelling east and west",e_w)
+
+while n_s > max_batch:
+    pass
+
+
 
